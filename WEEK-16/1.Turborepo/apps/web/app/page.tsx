0ca962a143c30c56799b91ae256c54e082256558
Agent-cat <2300031042@kluniversity.in>
2025-07-01 22:19:05 +0530

    name

--- WEEK-16/1.Turborepo/apps/web/app/page.tsx
+++ WEEK-16/1.Turborepo/apps/web/app/page.tsx
@@ -1,13 +1,14 @@
-import React from 'react'
-import { Button } from '@repo/ui/button'
+import React from "react";
+import { Button } from "@repo/ui/button";
 const page = () => {
-  return ( 
+  return (
     <div>
-      <Button className='vishnu'appName='vishnu' >
-      <h1>vishnu</h1>
+      <Button className="vishnu" appName="vishnu">
+        <h1>vishnu</h1>
+        <h1>Hi i am vishnu vardhan reddy</h1>
       </Button>
     </div>
-  )
-}
+  );
+};
 
-export default page
+export default page;
