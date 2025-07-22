5ca44d4e55aa833cf9bbe4f1709d2efbbc05455d
Agent-cat <2300031042@kluniversity.in>
2025-07-22 10:35:23 +0530

    recoil

---
+++ Week-7/7.4/1-Basics/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ButtomComponent from "./ButtomComponent";
+import { RecoilRoot } from "recoil";
+const App = () => {
+  return (
+    <RecoilRoot>
+      <ButtomComponent />
+    </RecoilRoot>
+  );
+};
+
+export default App;
